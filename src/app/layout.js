61ef172660842aabfea06c759a9df3ac366dd00a
Ashikur Rahaman Ahad <ashikurrahamanahad@gmail.com>
2026-08-15 21:03:13 +0600

    added speed insights component to layout.js and updated person-schema.js to personSchema.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { personSchema } from "@/data/person-schema";
+import { personSchema } from "@/data/personSchema";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
           }}
         />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
