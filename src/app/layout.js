193af099eae75e11567e2ef10dec24f8fa20d853
Ashikur Rahaman Ahad <ashikurrahamanahad@gmail.com>
2026-08-15 21:06:54 +0600

    added SpeedInsights component to the layout to monitor performance metrics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { personSchema } from "@/data/personSchema";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
